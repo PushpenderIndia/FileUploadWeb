65447484fa42eab9aab4140644e18810788c5c4f
Pushpender Singh <singhpushpender250@gmail.com>
2022-06-06 19:39:39 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FileUploadWeb
 Collection of Vulnerable Websites containing File Upload Vulnerability for practicing skill
+
+* Please move the `challenges` to `/var/www/html` folder
+* Then give executable permission to `uploads` folder using this cmd: `sudo chmod 777 /var/www/html/challenges/uploads`
+* Run Apache2 service: `sudo service apache2 start`
+* Visit this url in your web browser: `localhost/challenges`
---
+++ challenges/static/js/challenge2.js
@@ -0,0 +1,27 @@
+const dropArea = document.querySelector(".drop_box"),
+  button = dropArea.querySelector("button"),
+  dragText = dropArea.querySelector("header"),
+  input = dropArea.querySelector("input");
+let file;
+var filename;
+
+button.onclick = () => {
+  input.click();
+};
+
+input.addEventListener("change", function (e) {
+  var fileName = e.target.files[0].name;
+  let filedata = `
+    <form action="php/upload2.php" method="post" enctype="multipart/form-data">
+    <div class="form">
+    <h4>${fileName}</h4>
+    <br>
+    <button class="btn">Upload</button>
+    </div>
+    </form>`;
+  dropArea.innerHTML = filedata;
+});
+
+function cyberacademy() {
+  window.open("https://www.youtube.com/channel/UCRv-wp0CWtW2J33NkTId62w");
+}
